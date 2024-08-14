69900478c8ac70b2a5d5caa91be8b64b02267ef6
Shailesh Saravanan <77380733+Shaileshsaravanan@users.noreply.github.com>
2024-08-14 21:07:50 +0530

    more testing and training

--- load_model.py
+++ load_model.py
@@ -1,23 +1,30 @@
-# later...
-# - - - - - - - LOAD THE MODEL - - - - - - - -
- 
-# load json and create model
-from keras.models import model_from_json
-
-json_file = open('model.json', 'r')
-loaded_model_json = json_file.read()
-json_file.close()
-loaded_model = model_from_json(loaded_model_json)
-# load weights into new model
-loaded_model.load_weights("model.h5")
-print("Loaded model from disk")
- 
-# evaluate loaded model on test data
 import cv2
-image_test = cv2.imread('dataset/6c.jpg', 0)      # Predict images from dataset folder
-image_test_1 = cv2.resize(image_test, (28,28))    # For plt.imshow
-image_test_2 = image_test_1.reshape(1,1,28,28)    # For input to model.predict_classes
+import sys
+import numpy as np
+from tensorflow.keras import models
 
-#cv2.imshow('number', image_test_1)
-loaded_model_pred = loaded_model.predict_classes(image_test_2, verbose = 0)
-print('Prediction of loaded_model: {}'.format(loaded_model_pred[0]))
+MODEL_PATH = "tf-cnn-model.h5"
+
+def predict_digit(image_path):
+    
+    # load model
+    model = models.load_model(MODEL_PATH)
+    print("[INFO] Loaded model from disk.")
+
+    image = cv2.imread(image_path, 0)      
+    image1 = cv2.resize(image, (28,28))    # For cv2.imshow: dimensions should be 28x28
+    image2 = image1.reshape(1,28,28,1)
+
+    cv2.imshow('digit', image1 )
+    pred = np.argmax(model.predict(image2), axis=-1)
+    return pred[0]    
+
+def main(image_path):
+    predicted_digit = predict_digit(image_path)
+    print('Predicted Digit: {}'.format(predicted_digit))
+ 
+if __name__ == "__main__":
+    try:
+        main(image_path = sys.argv[1])
+    except:
+        print('[ERROR]: Image not found')
