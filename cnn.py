7f6bf45286b2285eb0702a19b76e58eaefc45f0a
Shailesh Saravanan <77380733+Shaileshsaravanan@users.noreply.github.com>
2024-08-14 18:17:27 +0530

    data set addition + more training

--- cnn.py
+++ cnn.py
@@ -1,89 +1,91 @@
+# Larger CNN for the MNIST Dataset
 import numpy
 import cv2
 from keras.datasets import mnist
 from keras.models import Sequential
-from keras.layers import Dense, Dropout, Flatten, Conv2D, MaxPooling2D
+from keras.layers import Dense
+from keras.layers import Dropout
+from keras.layers import Flatten
+from keras.layers.convolutional import Conv2D
+from keras.layers.convolutional import MaxPooling2D
 from keras.utils import np_utils
 from keras import backend as K
+import matplotlib.pyplot as plt
+
 K.set_image_dim_ordering('th')
 
+# Fix random seed for reproducibility
 seed = 7
 numpy.random.seed(seed)
 
 (X_train, y_train), (X_test, y_test) = mnist.load_data()
 
+# Reshape to be samples*pixels*width*height
 X_train = X_train.reshape(X_train.shape[0], 1, 28, 28).astype('float32')
 X_test = X_test.reshape(X_test.shape[0], 1, 28, 28).astype('float32')
 
+# Normalize inputs from 0-255 to 0-1
 X_train = X_train / 255
 X_test = X_test / 255
 
+# One Hot encode outputs
 y_train = np_utils.to_categorical(y_train)
 y_test = np_utils.to_categorical(y_test)
 num_classes = y_test.shape[1]
 
-def larger_model():
-	model = Sequential()
-	model.add(Conv2D(30, (5, 5), input_shape=(1, 28, 28), activation='relu'))
-	model.add(MaxPooling2D(pool_size=(2, 2)))
-	model.add(Conv2D(15, (3, 3), activation='relu'))
-	model.add(MaxPooling2D(pool_size=(2, 2)))
-	model.add(Dropout(0.2))
-	model.add(Flatten())
-	model.add(Dense(128, activation='relu'))
-	model.add(Dense(50, activation='relu'))
-	model.add(Dense(num_classes, activation='softmax'))
-	model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-	return model
+# Create model
+model = Sequential()
+model.add(Conv2D(32, (5, 5), input_shape=(1, 28, 28), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Conv2D(16, (3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.2))
+model.add(Flatten())
+model.add(Dense(128, activation='relu'))
+model.add(Dense(64, activation='relu'))
+model.add(Dense(num_classes, activation='softmax'))
 
-model = larger_model()
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=10, batch_size=200)
 
+# Final evaluation of the model
 scores = model.evaluate(X_test, y_test, verbose=0)
 print("Large CNN Error: %.2f%%" % (100-scores[1]*100))
 
-a1 = X_test[1]
-a2 = a1.reshape(1, 1, 28, 28)
-model_pred = model.predict_classes(a2, verbose=0)
-print('Prediction of loaded_model: {}'.format(model_pred[0]))
+#  - - - - - - - TEST single image - - - - - - - -
 
-import matplotlib.pyplot as plt
+image = (X_test[1]).reshape(1,1,28,28) # 1->'2';    
+model_pred = model.predict_classes(image, verbose = 0)
+print('Prediction of model: {}'.format(model_pred[0]))
 
-test_images = X_test[1:4]
+
+# - - - - - - TESTING multiple image - - - - - - - - - -
+
+test_images = X_test[1:5]
 test_images = test_images.reshape(test_images.shape[0], 28, 28)
-print("test images shape - {}".format(test_images.shape))
+print ("Test images shape: {}".format(test_images.shape))
 
 for i, test_image in enumerate(test_images, start=1):
-	org_image = test_image
-	test_image = test_image.reshape(1, 1, 28, 28)
-	prediction = model.predict_classes(test_image, verbose=0)
-	print("I think the digit is - {}".format(prediction[0]))
-	plt.subplot(220+i)
-	plt.imshow(org_image, cmap=plt.get_cmap('gray'))
+    org_image = test_image
+    test_image = test_image.reshape(1,1,28,28)
+    prediction = model.predict_classes(test_image, verbose=0)
+
+    print ("Predicted digit: {}".format(prediction[0]))
+    plt.subplot(220+i)
+    plt.axis('off')
+    plt.title("Predicted digit: {}".format(prediction[0]))
+    plt.imshow(org_image, cmap=plt.get_cmap('gray'))
 
 plt.show()
 
+
+# - - - - - - - SAVE THE MODEL - - - - - - - -
+
+# serialize model to JSON
 model_json = model.to_json()
 with open("model.json", "w") as json_file:
     json_file.write(model_json)
-
+# serialize weights to HDF5
 model.save_weights("model.h5")
 print("Saved model to disk")
-
-from keras.models import model_from_json
-
-json_file = open('model.json', 'r')
-loaded_model_json = json_file.read()
-json_file.close()
-loaded_model = model_from_json(loaded_model_json)
-loaded_model.load_weights("model.h5")
-print("Loaded model from disk")
-
-(trainData, trainLabels), (testData, testLabels) = mnist.load_data()
-image_test = cv2.imread('images/5a.jpg', 0)
-image_test_1 = cv2.resize(image_test, (28, 28))
-image_test_2 = image_test_1.reshape(1, 1, 28, 28)
-
-cv2.imshow('number', image_test_1)
-loaded_model_pred = loaded_model.predict_classes(image_test_2, verbose=0)
-print('Prediction of loaded_model: {}'.format(loaded_model_pred[0]))
+ 
